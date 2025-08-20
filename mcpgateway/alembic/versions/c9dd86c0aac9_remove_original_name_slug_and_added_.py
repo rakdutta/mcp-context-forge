2461d7d0cbf9621eb9bac17c87e763cca00de30d
RAKHI DUTTA <rakdutta@in.ibm.com>
2025-08-20 11:17:17 +0530

    flake8 fix
    Signed-off-by: RAKHI DUTTA <rakdutta@in.ibm.com>

--- mcpgateway/alembic/versions/c9dd86c0aac9_remove_original_name_slug_and_added_.py
+++ mcpgateway/alembic/versions/c9dd86c0aac9_remove_original_name_slug_and_added_.py
@@ -5,15 +5,17 @@ Revises: add_oauth_tokens_table
 Create Date: 2025-08-19 15:15:26.509036
 
 """
+
+# Standard
 from typing import Sequence, Union
 
+# Third-Party
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = 'c9dd86c0aac9'
-down_revision: Union[str, Sequence[str], None] = 'add_oauth_tokens_table'
+revision: str = "c9dd86c0aac9"
+down_revision: Union[str, Sequence[str], None] = "add_oauth_tokens_table"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -21,18 +23,18 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     # Remove original_name_slug column
-    op.drop_column('tools', 'original_name_slug')
-    
-        # Add custom_name column
-    op.add_column('tools', sa.Column('custom_name', sa.String(), nullable=True))
+    op.drop_column("tools", "original_name_slug")
+
+    # Add custom_name column
+    op.add_column("tools", sa.Column("custom_name", sa.String(), nullable=True))
     # ### end Alembic commands ###
-    
+
+
 def downgrade() -> None:
     """Downgrade schema."""
     # Remove custom_name column
-    op.drop_column('tools', 'custom_name')
+    op.drop_column("tools", "custom_name")
 
     # Add original_name_slug column back
-    op.add_column('tools', sa.Column('original_name_slug', sa.String(), nullable=True))
+    op.add_column("tools", sa.Column("original_name_slug", sa.String(), nullable=True))
     # ### end Alembic commands ###
-    
